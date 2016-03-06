6fc1dd637a0fd9592e603af3ea4cdbfb6321092b
Doron Behar <doron.behar@gmail.com>
2016-03-06 13:16:29 +0200

    still state_machine doesn't drive logic

--- reader.vhd
+++ reader.vhd
@@ -5,116 +5,109 @@ use IEEE.std_logic_arith.all;
 
 entity RFID_reader is
 	port	(
-		reset	:in std_logic;
-		clk50mhz:in std_logic;
-		not_data:in std_logic;
-		ID		:out std_logic_vector(40-1 downto 0)
+		reset			:in std_logic;
+		clk50mhz		:in std_logic;
+		RFID_not_data	:in std_logic;
+		ID				:out std_logic_vector(40-1 downto 0);
+		successful		:out std_logic
 	);
 end entity;
 
 architecture arc of RFID_reader is
-signal data:std_logic;
-type main_state_type is (wait2startbits,startbits,reading,wait2endbits,endbits);
-signal main_state:main_state_type;
-
+function ascii2hex(code:std_logic_vector(6 downto 0)) return std_logic_vector is
+begin
+	if code<x"3a" and code>=x"2F" then
+		return code-x"30";
+	elsif code<x"47" and code>x"40" then
+		return code-x"41";
+	end if;
+end function;
 component PLL50mhz_2400hz
 	port	(	areset	:in std_logic:='0';
 				inclk0	:in std_logic:='0';
 				c0		:out std_logic		);
 end component;
+signal data		:std_logic;
 signal clk2400hz:std_logic;
-
-constant IDRL	:integer range 0 to 100:=100;--ID-register length--10 bits per 10 bytes
-signal BC		:integer range 0 to IDRL-1;--bits counter
-signal RC		:std_logic;--'read-count' - counts how many times the ID was read
-constant IDL	:integer range 0 to 40:=40;--ID string length
-subtype ID_str is std_logic_vector(IDRL downto 0);
-signal IDR		:ID_str;--read-ID register
-constant EBL	:integer range 0 to 9:=9;--edges' bytes' length including stop bits
-subtype RFID_byte is std_logic_vector(EBL-1 downto 0);
-constant SB		:RFID_byte:="100001010";--start bits constant
-constant EB		:RFID_byte:="100001101";--end bits constant
-signal char		:RFID_byte;--unique ID char register
-begin--both constants include the stop bits -- '1'
-	data<=not not_data;
+begin
+	data<=not RFID_not_data;
 	PLL50mhz_2400hz_inst:PLL50mhz_2400hz
 		port map(
 			inclk0=>clk50mhz,
 			areset=>not reset,
 			c0=>clk2400hz
 		);
-	reader:process(clk2400hz,reset,data)
-	variable IDV:ID_str;--read-ID variable
+	process(clk2400hz,reset,data)
+	variable tmp:std_logic_vector(7 downto 0);
+	variable counter:integer range 0 to 7;
+	type main_state_type is (wait4startbits,startbits,reading,wait4endbits,endbits);
+	variable main_state:main_state_type;
+	variable byte:integer range 0 to 9;
 	begin
 		if reset='0' then
-			RC<='0';
-			main_state<=wait2startbits;
-			BC<=0;
+			main_state:=wait4startbits;
+			byte:=0;
+			counter:=0;
+			tmp:=(others=>'0');
+			ID<=(others=>'0');
+			successful<='0';
 		elsif rising_edge(clk2400hz) then
 			case main_state is
-			when wait2startbits=>
-				if data='0' then--start bit
-					main_state<=startbits;
+			when wait4startbits=>
+				if data='0' then--start-bit
+					main_state:=startbits;
+					successful<='0';
 				end if;
 			when startbits=>
-				if BC<EBL-1 then--if BC<8
-					char(BC)<=data;
-					BC<=BC+1;
-				else--BC=8 then
-					char(EBL-1)<=data;--char(8)<=data;
-					BC<=0;
-					if char=SB then
-						main_state<=reading;
+				if counter<8 then
+					tmp(counter):=data;
+					counter:=counter+1;
+				else
+					counter:=0;
+					if tmp=x"0a" and data='1' then--data='1' is stop-bit
+						main_state:=reading;
 					else
-						main_state<=wait2startbits;
+						main_state:=wait4startbits;
 					end if;
-				end if; 
-			when reading=>
-				if BC<IDRL then--if BC<100
-					IDV(BC):=data;
-					BC<=BC+1;
-				else--BC=99 then
-					IDV(IDRL):=data;--IDV(100):=data when IDRL=100
-					BC<=0;
-					main_state<=wait2endbits;
 				end if;
-			when wait2endbits=>
+			when reading=>
+				if counter<8 then
+					tmp(counter):=data;
+					counter:=counter+1;
+				else
+					counter:=0;
+					if data='1' then--data='1'
+						ID(byte*4+3 downto byte*4)<=ascii2hex(tmp);
+						if byte<9 then
+							byte:=byte+1;
+						else
+							byte:=0;
+							main_state:=wait4endbits;
+						end if;
+					else
+						main_state:=wait4startbits;
+					end if;
+				end if;
+			when wait4endbits=>
 				if data='0' then
-					main_state<=endbits;
+					main_state:=endbits;
+				else
+					main_state:=wait4startbits;
 				end if;
 			when endbits=>
-				if BC<EBL-1 then
-					char(BC)<=data;
-					BC<=BC+1;
-				else--BC=8-1=7
-					char(EBL-1)<=data;
-					BC<=0;
-					if char=EB then
-						IDR<=IDV;
+				if counter<8 then
+					tmp(counter):=data;
+					counter:=counter+1;
+				else
+					counter:=0;
+					if tmp=x"0d" and data='1' then--data='1' is stop-bit
+						successful<='1';
 					else
-						IDR<=(others=>'0');
+						successful<='0';
 					end if;
-					main_state<=wait2startbits;
+					main_state:=wait4startbits;
 				end if;
 			end case;
 		end if;
-	end process reader;
-	process(IDR)
-	variable IDRNA:std_logic_vector(7 downto 0);--ID register not ascii (after decrementation by x"30")
-	variable i,j:integer range 0 to IDRL;
-	variable x:integer range 0 to IDL;
-	begin
-		i:=1;j:=8;
-		x:=0;
-		while j<IDRL-1 and x<IDL-1 loop
-			IDRNA:=IDR(j downto i)-x"30";
-			i:=i+9;j:=j+9;
-			ID(x+3 downto x)<=IDRNA(3 downto 0);
-			x:=x+4;
-		end loop;
 	end process;
---	memory:process(reset,clk50mhz)
---	begin
---		
---	end process;
 end arc;
