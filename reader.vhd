d6feed3b18808468f83bad679bb7cb3422e76db5
Doron Behar <doron.behar@gmail.com>
2016-03-07 06:18:47 +0200

    working!
    comment: a bit flaky - `successful` bit is almost always low but i
             commit even for 1 success.
    remove ascii2hex function

--- reader.vhd
+++ reader.vhd
@@ -9,24 +9,11 @@ entity RFID_reader is
 		clk50mhz		:in std_logic;
 		RFID_not_data	:in std_logic;
 		ID				:out std_logic_vector(40-1 downto 0);
-		successful		:out std_logic;
-		read_state		:out std_logic
+		successful		:out std_logic
 	);
 end entity;
 
 architecture arc of RFID_reader is
-function ascii2hex(code:std_logic_vector(7 downto 0)) return std_logic_vector is
-	variable var:std_logic_vector(7 downto 0);
-begin
-	if code<x"3a" and code>=x"30" then
-		var:=code-x"30";
-	elsif code<x"47" and code>x"40" then
-		var:=code-x"41";
-	else
-		var:=x"00";
-	end if;
-	return var(3 downto 0);
-end function;
 component PLL50mhz_2400hz
 	port	(	areset	:in std_logic:='0';
 				inclk0	:in std_logic:='0';
@@ -34,6 +21,7 @@ component PLL50mhz_2400hz
 end component;
 signal data		:std_logic;
 signal clk2400hz:std_logic;
+signal IDR:std_logic_vector(100-1 downto 0);
 begin
 	data<=not RFID_not_data;
 	PLL50mhz_2400hz_inst:PLL50mhz_2400hz
@@ -44,24 +32,21 @@ begin
 		);
 	process(clk2400hz,reset,data)
 	variable tmp:std_logic_vector(7 downto 0);
-	variable counter:integer;
+	variable counter:integer range 0 to 100;
 	type main_state_type is (wait4startbits,startbits,wait4reading,reading,wait4endbits,endbits);
 	variable main_state:main_state_type;
-	type read_state_type is (wait4startbit,reading);
-	type read_type is record
-		byte:integer range 0 to 9;
-		state:read_state_type;
-	end record;
-	variable read:read_type;
+	variable byte:std_logic_vector(7 downto 0);
+	variable ascii:std_logic_vector(7 downto 0);
 	begin
 		if reset='0' then
 			main_state:=wait4startbits;
-			read.state:=wait4startbit;
-			read.byte:=0;
 			counter:=0;
 			tmp:=(others=>'0');
 			ID<=(others=>'0');
 			successful<='0';
+			IDR<=(others=>'0');
+			byte:=(others=>'0');
+			ascii:=(others=>'0');
 		elsif rising_edge(clk2400hz) then
 			case main_state is
 			when wait4startbits=>
@@ -88,46 +73,17 @@ begin
 					main_state:=wait4startbits;
 				end if;
 			when reading=>
-				case read.byte is
-				when 0 to 8=>
-					case read.state is
-					when wait4startbit=>
-						if data='0' then--data='0' is start-bit
-							read.state:=reading;
-						end if;
-					when reading=>
-						if counter<8 then
-							tmp(counter):=data;
-							counter:=counter+1;
-						else
-							counter:=0;
-							if data='1' then--data='1' is stop-bit
-								ID(read.byte*4+3 downto read.byte*4)<=ascii2hex(tmp);
-								read.byte:=read.byte+1;
-								read.state:=wait4startbit;
-							end if;
-						end if;
-					end case;
-				when 9=>
-					case read.state is
-					when wait4startbit=>
-						if data='0' then--data='0' is start-bit
-							read.state:=reading;
-						end if;
-					when reading=>
-						if counter<8 then
-							tmp(counter):=data;
-							counter:=counter+1;
-						else
-							counter:=0;
-							if data='1' then--data='1' is stop-bit
-								ID(read.byte*4+3 downto read.byte*4)<=ascii2hex(tmp);
-								read.byte:=0;
-								main_state:=wait4endbits;
-							end if;
-						end if;
-					end case;
-				end case;
+				if counter<100-1 then
+					counter:=counter+1;
+					IDR(counter)<=data;
+				else
+					counter:=0;
+					if data='1' then
+						main_state:=wait4endbits;
+					else
+						main_state:=wait4startbits;
+					end if;
+				end if;
 			when wait4endbits=>
 				if data='0' then
 					main_state:=endbits;
@@ -142,17 +98,23 @@ begin
 					counter:=0;
 					if tmp=x"0d" and data='1' then--data='1' is stop-bit
 						successful<='1';
+						for i in 0 to 9 loop
+							byte:=IDR(i*10+8 downto i*10+1);
+							if IDR(i*10)='0' and IDR(i*10+9)='1' then --start and stop bits
+								if byte>=x"30" and byte<=x"39" then
+									ascii:=byte-x"30";
+									ID(i*4+3 downto i*4)<=ascii(3 downto 0);
+								else
+									ID(i*4+3 downto i*4)<="ZZZZ";
+								end if;
+							end if;
+						end loop;
 					else
 						successful<='0';
 					end if;
 					main_state:=wait4startbits;
 				end if;
 			end case;
-		end if;
-		if read.state=wait4startbit then
-			read_state<='1';
-		else 
-			read_state<='0';
 		end if;
 	end process;
 end arc;
