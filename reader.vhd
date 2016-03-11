e1645aaa83a5549b7b26365ba0427973b49004c0
Doron Behar <doron.behar@gmail.com>
2016-03-11 17:51:45 +0200

    try to fix the sampling
    add new state to `main_state`
    add function to detect valid samples
    add function to convert from ascii code to hex

--- reader.vhd
+++ reader.vhd
@@ -14,14 +14,6 @@ entity RFID_reader is
 end entity;
 
 architecture arc of RFID_reader is
-function ascii2hex(code:std_logic_vector(6 downto 0)) return std_logic_vector is
-begin
-	if code<x"3a" and code>=x"2F" then
-		return code-x"30";
-	elsif code<x"47" and code>x"40" then
-		return code-x"41";
-	end if;
-end function;
 component PLL50mhz_2400hz
 	port	(	areset	:in std_logic:='0';
 				inclk0	:in std_logic:='0';
@@ -39,81 +31,142 @@ begin
 		);
 	process(clk2400hz,reset,data)
 	variable tmp:std_logic_vector(7 downto 0);
-	variable counter:integer range 0 to 7;
-	type main_state_type is (wait4startbits,startbits,wait4char,reading,wait4endbits,endbits);
+	variable counter:integer range 0 to 100;
+	type main_state_type is (wait4startbits,startbits,wait4byte,reading,wait4endbits,endbits,fixing);
 	variable main_state:main_state_type;
-	variable byte:integer range 0 to 9;
+	type byte_type is array(3 downto 0,7 downto 0) of std_logic_vector(7 downto 0);
+	variable byte:byte_type;
+	variable sample:integer range 0 to 3;
+	variable index:integer range 0 to 7;
+	subtype var_type is std_logic_vector(110-1 downto 0);
+	variable var:var_type;
+	function valid(chr:std_logic_vector(9 downto 0)) return boolean is
+	begin
+		if chr(9)='1' and ((chr(8 downto 1)>=x"30" and chr(8 downto 1)<=x"39") or (chr(8 downto 1)>=x"41" and chr(8 downto 1)<=x"46")) and chr(0)='0' then
+			return true;
+		else
+			return false;
+		end if;
+	end function;
+	function ascii2hex(chr:std_logic_vector(7 downto 0)) return std_logic_vector is
+		variable x:std_logic_vector(7 downto 0);
+	begin
+		if chr>=x"30" and chr<=x"39" then
+			x:=chr-x"30";
+		elsif chr>=x"41" and chr<=x"46" then
+			x:=chr-x"41"+10;
+		else
+			x:=(others=>'Z');
+		end if;
+		return x(3 downto 0);
+	end function;
 	begin
 		if reset='0' then
 			main_state:=wait4startbits;
-			byte:=0;
 			counter:=0;
 			tmp:=(others=>'0');
 			ID<=(others=>'0');
 			successful<='0';
+--			sample:=0;
+			index:=0;
+			byte:=(others=>(others=>(others=>'0')));
+			var:=(others=>'0');
 		elsif rising_edge(clk2400hz) then
 			case main_state is
-			when wait4startbits=>
-				if data='0' then--start-bit
-					main_state:=startbits;
-					successful<='0';
-				end if;
-			when startbits=>
-				if counter<8 then
-					tmp(counter):=data;
-					counter:=counter+1;
-				else
-					counter:=0;
-					if tmp=x"0a" and data='1' then--data='1' is stop-bit
-						main_state:=wait4char;
-					else
-						main_state:=wait4startbits;
-					end if;
-				end if;
-			when wait4char=>
-				if data='0' then
-					main_state:=reading;
-				else
-					main_state:=wait4startbits;
-				end if;
-			when reading=>
-				if counter<8 then
-					tmp(counter):=data;
-					counter:=counter+1;
-				else
-					counter:=0;
-					if data='1' then--data='1'
-						ID(byte*4+3 downto byte*4)<=ascii2hex(tmp);
-						if byte<9 then
-							main_state:=wait4char;
-							byte:=byte+1;
-						else
-							main_state:=wait4endbits;
-							byte:=0;
-						end if;
-					else
-						main_state:=wait4startbits;
-					end if;
-				end if;
-			when wait4endbits=>
-				if data='0' then
-					main_state:=endbits;
-				else
-					main_state:=wait4startbits;
-				end if;
-			when endbits=>
-				if counter<8 then
-					tmp(counter):=data;
-					counter:=counter+1;
-				else
-					counter:=0;
-					if tmp=x"0d" and data='1' then--data='1' is stop-bit
-						successful<='1';
-					else
+				when wait4startbits=>
+					if data='0' then--start-bit
+						main_state:=startbits;
 						successful<='0';
+						var:=(others=>'0');
+						tmp:=(others=>'0');
+						index:=0;
 					end if;
-					main_state:=wait4startbits;
-				end if;
+				when startbits=>
+					if counter<8 then
+						tmp(counter):=data;
+						counter:=counter+1;
+					else
+						counter:=0;
+						if tmp=x"0a" and data='1' then--data='1' is stop-bit
+							main_state:=wait4byte;
+						else
+							main_state:=wait4startbits;
+						end if;
+					end if;
+				when wait4byte=>
+					if data='0' then
+						main_state:=reading;
+					else
+						main_state:=fixing;
+						counter:=index*10+9;
+						index:=0;
+					end if;
+				when reading=>
+					if counter<8 then
+						tmp(counter):=data;
+						counter:=counter+1;
+					else
+						counter:=0;
+						if valid(data&tmp&'0')=true then
+							byte(0,index):=tmp;
+							if index<8 then
+								index:=index+1;
+								main_state:=wait4byte;
+							else
+								index:=0;
+								main_state:=wait4endbits;
+							end if;
+						else
+							var(index*10+0):='0';
+							var(index*10+8 downto index*10+1):=tmp;
+							counter:=index*10+9;
+							index:=0;
+							main_state:=fixing;--state ment to deal with overlapping data
+						end if;
+					end if;
+				when wait4endbits=>
+					if data='0' then
+						main_state:=endbits;
+					else
+						main_state:=wait4startbits;
+					end if;
+				when endbits=>
+					if counter<8 then
+						tmp(counter):=data;
+						counter:=counter+1;
+					else
+						counter:=0;
+						if tmp=x"0D" and data='1' then
+							for i in 0 to 7 loop--check all bytes;
+								for j in 0 to 3 loop--check if the current sample's byte is equal to the corresponding one in another sample;
+									if byte(0,i)=byte((0+j) mod 4,i) then
+										ID(i*4+3 downto i*4)<=ascii2hex(byte(0,i));
+										exit;
+									end if;
+								end loop;
+							end loop;
+--							sample:=sample+1;
+						end if;
+						index:=0;
+						main_state:=wait4startbits;
+					end if;
+				when fixing=>
+					if counter<110 then
+						var(counter):=data;
+						counter:=counter+1;
+					else
+						counter:=0;
+						if var(110-1 downto 110-10)='0'&x"0D"&'1' then
+							for i in 9 downto 0 loop
+								if valid(var(i*10+9 downto i*10))=true then
+									ID(i*4+3 downto i*4)<=ascii2hex(var(i*10+8 downto i*10+1));
+								else
+									exit;
+								end if;
+							end loop;
+						end if;
+						main_state:=wait4startbits;
+					end if;
 			end case;
 		end if;
 	end process;
