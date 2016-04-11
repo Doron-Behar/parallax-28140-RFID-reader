11cbfc293477ef14c0ba0dc2cc609c83a1360a8c
Doron Behar <doron.behar@gmail.com>
2016-04-11 21:49:15 +0300

    small changes
    fix indentations before process begins
    fix sesitivity list of process to exclude `data`.
    use `others` in case

--- reader.vhd
+++ reader.vhd
@@ -15,13 +15,15 @@ entity RFID_reader is
 end entity;
 
 architecture arc of RFID_reader is
-component PLL50mhz_2400hz
-	port	(	areset	:in std_logic:='0';
-				inclk0	:in std_logic:='0';
-				c0		:out std_logic		);
-end component;
-signal data		:std_logic;
-signal clk2400hz:std_logic;
+	component PLL50mhz_2400hz
+		port(
+			areset	:in std_logic:='0';
+			inclk0	:in std_logic:='0';
+			c0		:out std_logic
+		);
+	end component;
+	signal data		:std_logic;
+	signal clk2400hz:std_logic;
 begin
 	data<=not not_data;
 	PLL50mhz_2400hz_inst:PLL50mhz_2400hz
@@ -30,27 +32,27 @@ begin
 			areset=>not reset,
 			c0=>clk2400hz
 		);
-	process(clk2400hz,reset,data)
-	function valid(chr:std_logic_vector(9 downto 0)) return boolean is
-	begin
-		if chr(9)='1' and ((chr(8 downto 1)>=x"30" and chr(8 downto 1)<=x"39") or (chr(8 downto 1)>=x"41" and chr(8 downto 1)<=x"46")) and chr(0)='0' then
-			return true;
-		else
-			return false;
-		end if;
-	end function;
-	function ascii2hex(chr:std_logic_vector(7 downto 0)) return std_logic_vector is
-		variable x:std_logic_vector(7 downto 0);
-	begin
-		if chr>=x"30" and chr<=x"39" then
-			x:=chr-x"30";
-		elsif chr>=x"41" and chr<=x"46" then
-			x:=chr-x"41"+10;
-		else
-			x:=(others=>'Z');
-		end if;
-		return x(3 downto 0);
-	end function;
+	process(clk2400hz,reset)
+		function valid(chr:std_logic_vector(9 downto 0)) return boolean is
+		begin
+			if chr(9)='1' and ((chr(8 downto 1)>=x"30" and chr(8 downto 1)<=x"39") or (chr(8 downto 1)>=x"41" and chr(8 downto 1)<=x"46")) and chr(0)='0' then
+				return true;
+			else
+				return false;
+			end if;
+		end function;
+		function ascii2hex(chr:std_logic_vector(7 downto 0)) return std_logic_vector is
+			variable x:std_logic_vector(7 downto 0);
+		begin
+			if chr>=x"30" and chr<=x"39" then
+				x:=chr-x"30";
+			elsif chr>=x"41" and chr<=x"46" then
+				x:=chr-x"41"+10;
+			else
+				x:=(others=>'Z');
+			end if;
+			return x(3 downto 0);
+		end function;
 		variable tmp:std_logic_vector(7 downto 0);
 		variable counter:integer range 0 to 100;
 		type main_state_type is (wait4startbits,startbits,wait4byte,reading,wait4endbits,endbits,fixing);
@@ -58,19 +60,19 @@ begin
 		variable sample:integer range 0 to 3;
 		variable index:integer range 0 to 9;
 		variable err:main_state_type;
-	procedure assign(
-		byte:in std_logic_vector(7 downto 0)
-	) is
-	begin
-		ID(index*4+3 downto index*4)<=ascii2hex(byte);
-		if index<9 then
-			index:=index+1;
-			main_state:=wait4byte;
-		else
-			index:=0;
-			main_state:=wait4endbits;
-		end if;
-	end procedure assign;
+		procedure assign(
+			byte:in std_logic_vector(7 downto 0)
+		) is
+		begin
+			ID(index*4+3 downto index*4)<=ascii2hex(byte);
+			if index<9 then
+				index:=index+1;
+				main_state:=wait4byte;
+			else
+				index:=0;
+				main_state:=wait4endbits;
+			end if;
+		end procedure assign;
 	begin
 		if reset='0' then
 			main_state:=wait4startbits;
@@ -147,7 +149,7 @@ begin
 						main_state:=wait4startbits;
 						broadcast<='0';
 					end if;
-				when fixing=>
+				when others=>
 					if err=reading then
 						--check if the byte that was recieved is in a proper range:
 						if valid(data&tmp&'0')=true then
